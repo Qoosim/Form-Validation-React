83a49d3c39fa4772b50fe5c8e27292c4f49c4a5a
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-01-15 15:28:56 +0100

    Create App2 component to handle another form validation that trigger the validation method when the form gets submitted

---
+++ src/App2.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+class App2 extends Component {
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            age: null
+        }
+    }
+
+    mySubmitHandler = (event) => {
+        event.preventDefault()
+        let age = this.state.age
+        if (!Number(age)) {
+            alert("Age must be a number")
+        }
+    }
+
+    handleChange = (event) => {
+        let name = event.target.name
+        let value = event.target.value
+
+        this.setState({
+            [name]: value
+        })
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-4 offset-4">
+                        <form onSubmit={this.mySubmitHandler}>
+                            <h2>Hello {this.state.username} {this.state.age}</h2>
+                            <label>Username:</label>
+                            <input 
+                                className="form-control"
+                                type="text" 
+                                name="username"
+                                placeholder="Username"
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <input 
+                                className="form-control"
+                                type="" 
+                                name="age"
+                                placeholder="Age"
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <button className="btn btn-info">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App2
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import App2 from './App2'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      {/*<App />*/}
+      <App2 />
   </React.StrictMode>,
   document.getElementById('root')
 );
