1935774ef7b129aa5f94910d08a6e5e3d9815be9
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-01-15 14:59:13 +0100

    Complete the simple form validation for age

--- src/App.js
+++ src/App.js
@@ -9,13 +9,44 @@ class App extends Component {
             age: null
         }
     }
+
+    handleChange = (event) => {
+        let name = event.target.name
+        let value = event.target.value
+
+        if (name === "age") {
+            if (!Number(value)) {
+                alert('Age must be a number')
+            }
+        }
+        this.setState({
+            [name]: value
+        })
+    }
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <form>
-
-                    </form>
+                    <div className="col-3 offset-4">
+                        <form>
+                            <h2>Hey {this.state.username} {this.state.age}</h2>
+                            <input 
+                                className="form-control"
+                                type="text" 
+                                name="username"
+                                placeholder="Username"
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <input 
+                                className="form-control"
+                                type="text" 
+                                name="age"
+                                placeholder="age"
+                                onChange={this.handleChange}
+                            />
+                        </form>
+                    </div>
                 </div>
             </div>
         )
